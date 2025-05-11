0baacb1d82ede37463236ed0ef79b0d127f5c884
JonathanGarcia <martingarciayoni@gmail.com>
2025-05-10 23:21:25 -0300

    Definicion de escenarios y creacion de entorno de desarrollo

---
+++ sistema-negocio/src/test/java/com/sistemaumma/sistemanegocio/SistemaNegocioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistemaumma.sistemanegocio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaNegocioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
